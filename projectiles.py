d6082c9ed582ce58b4f6e5bc477f3abc498c9dc7
ConnorWolfe <Cwolfe25@gcds.net>
2024-04-24 10:56:18 -0400

    Update projectiles.py

--- projectiles.py
+++ projectiles.py
@@ -1,24 +1,30 @@
 import math 
 from polynomial import polynomial
 import matplotlib.pyplot as plt
-
 gravity = -9.8
 degree = input("what do you want the launch degree to be? ")
+
 launchv = input("what do you want the lauch velocity to be? ")
 displacementy = 0 
 degree = float(degree)
 launchv = float(launchv)
+degree = degree * 3.141592653589793238
+degree = degree / 180
+#degree = degree * 3.141592653589793238
+#degree = degree / 180
 yv = math.sin(degree)
 yv = abs(yv)
 yv = yv * launchv
 xv = math.cos(degree)
 xv = abs(xv)
 xv = xv * launchv
-equation = [-9.8, yv, 0]
+print(xv)
+print(yv)
+equation = [-4.9, yv, 0]
 eqx = [xv,0]
 flightx = polynomial(eqx)
 flight = polynomial(equation)
-time = yv / 9.8
+time = yv / 4.9
 print(time)
 #time = math.sqrt(time)
 displacementx = xv * time
